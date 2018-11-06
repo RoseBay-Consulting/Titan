1b88cba1d28459ca320ff2983bc8a7d95bcf1d03
Tilak <tilakpeace0000@gmail.com>
2018-11-06 11:23:04 +0545

    some test file added

--- test/main.go
+++ test/main.go
@@ -1,6 +1,36 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Starting the application...")
+
+	jsonData := map[string]string{"test": "I am Tilak"}
+	jsonValue, _ := json.Marshal(jsonData)
+	response, err := http.Post("https://dm0fpx25wg.execute-api.ap-southeast-1.amazonaws.com/test/check", "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
+		var responseObject Response
+		json.Unmarshal(responseData, &responseObject)
+
+	}
+
+	fmt.Println("Terminating the application...")
+}
+
+
+/*package main
+
+import (
 	"fmt"
 	"os"
 	"bufio"
@@ -23,4 +53,4 @@ func main() {
 	for line, n := range counts {
 		fmt.Printf("%d\t%s\n", n, line)
 	}
-}
+}*/
--- test/test.go
+++ test/test.go
@@ -1,1 +1,60 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"bytes"
+)
+
+// A Response struct to map the Entire Response
+type Response struct {
+	Status    string    `json:"status"`
+	Test    string    `json:"test"`
+	Message    string    `json:"message"`
+//	Pokemon []Pokemon `json:"pokemon_entries"`
+}
+
+
+
+// A Pokemon Struct to map every pokemon to.
+/*type Pokemon struct {
+	EntryNo int            `json:"entry_number"`
+	Species PokemonSpecies `json:"pokemon_species"`
+}
+*/
+// A struct to map our Pokemon's Species which includes it's name
+/*type PokemonSpecies struct {
+	Name string `json:"name"`
+}*/
+
+func main() {
+	jsonData := map[string]string{"test": "I am Tilak"}
+	jsonValue, _ := json.Marshal(jsonData)
+	response, err := http.Post("https://dm0fpx25wg.execute-api.ap-southeast-1.amazonaws.com/test/check", "application/json", bytes.NewBuffer(jsonValue))
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseObject Response
+	json.Unmarshal(responseData, &responseObject)
+
+	fmt.Println(responseObject.Test)
+	fmt.Println(len(responseObject.Status))
+//	fmt.Println(len(responseObject.Message))
+
+	/*for i := 0; i < len(responseObject.Pokemon); i++ {
+		fmt.Println(responseObject.Pokemon[i].Species.Name)
+	}
+*/
+}
